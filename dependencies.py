091f52ff88264655c3363d27ff5e39f0ddb252b8
anarghya-12 <166636310+anarghya-12@users.noreply.github.com>
2025-12-14 00:08:38 +0530

    updation and installation

---
+++ dependencies.py
@@ -0,0 +1,4 @@
+!apt-get update
+!apt-get install -y chromium-chromedriver
+!pip install selenium
+!pip install streamlit pyngrok joblib scikit-learn numpy pandas requests beautifulsoup4 scipy nltk requests_html pyppeteer
